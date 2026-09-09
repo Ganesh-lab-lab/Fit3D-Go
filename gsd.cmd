598a9a8196900ee6cabbb4729b920e992b7490a3
K.Ganesh <kethireddig@gmail.com>
2026-09-10 01:48:45 +0530

    chore: add root gsd-sdk and gsd launchers for IDE terminal access

---
+++ gsd.cmd
@@ -0,0 +1,2 @@
+@ECHO OFF
+@"C:\Users\SCTS\AppData\Local\Programs\Python\Python314\node.exe" "C:\Users\SCTS\AppData\Local\npm-cache\_npx\9785a834b31d581d\node_modules\get-shit-done-cc\bin\gsd-sdk.js" %*
